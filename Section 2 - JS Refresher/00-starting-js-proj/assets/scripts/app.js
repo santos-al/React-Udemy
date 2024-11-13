42d13e08232450a2907ccc589daaa5c9c3562176
Xandi <santosal2018@gmail.com>
2024-11-13 11:03:06 -0500

    complete react section 2

--- Section 2 - JS Refresher/00-starting-js-proj/assets/scripts/app.js
+++ Section 2 - JS Refresher/00-starting-js-proj/assets/scripts/app.js
@@ -14,3 +14,58 @@ const user = {
     name: "Alex",
     age: 24
 };
+
+const hobbies = [
+    "Sports",
+    "Cooking",
+    "Reading"
+];
+
+const index = hobbies.findIndex((item) => {
+    return item === "Cooking"
+});
+
+const newHobbies = hobbies.map((item) => {
+    return {text: item}
+})
+
+console.log(index);
+console.log(newHobbies);
+
+
+const [firstName, lastName] = ["Alex", "Santos"];
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+console.log(firstName);
+console.log(lastName);
+
+const {name: userName, age} = {
+    name: "Alexandre",
+    age: 24
+}
+
+console.log(userName);
+console.log(age);
+
+const newHobby = ["Baking", "Teaching"];
+
+// Adds the items as comma seperated values into the list
+const mergedHobbies = [...hobbies, ...newHobby];
+const combinedHobbies = [hobbies, newHobby];
+
+
+console.log(hobbies);
+console.log(mergedHobbies);
+console.log(combinedHobbies);
+
+// The spread operator in the is case takes all the key value pairs
+// from "user" and adds it to the new object
+const extendedUser = {
+    isAdmin: true,
+    ...user
+}
+
+
+setTimeout(() => {}, 2000)
