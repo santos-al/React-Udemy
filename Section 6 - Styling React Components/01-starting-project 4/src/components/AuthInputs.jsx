cf4e5311d6df7b41ac9ddf28e871a411a5dae90c
Xandi <santosal2018@gmail.com>
2024-12-02 15:10:35 -0500

    styled components practice

--- Section 6 - Styling React Components/01-starting-project 4/src/components/AuthInputs.jsx
+++ Section 6 - Styling React Components/01-starting-project 4/src/components/AuthInputs.jsx
@@ -1,4 +1,15 @@
 import { useState } from "react";
+import { styled } from "styled-components";
+
+import Input from "./Input";
+import Button from "./Button";
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -22,33 +33,31 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label className={`label ${emailNotValid ? "invalid" : undefined}`}>Email</label>
-          <input
+      <ControlContainer>
+          <Input
+            label="Email"
+            invalid={emailNotValid}
             type="email"
             className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
-        </p>
-        <p>
-          <label className={`label ${emailNotValid ? "invalid" : undefined}`}>Password</label>
-          <input
+          <Input
+            label="Password"
+            invalid={passwordNotValid}
             type="password"
             className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
-        </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
