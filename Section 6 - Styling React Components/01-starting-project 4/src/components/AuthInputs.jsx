11db3d6b3586304fe1e13f76100d3c756db2145e
Xandi <santosal2018@gmail.com>
2024-12-03 14:36:12 -0500

    complete section 6

--- Section 6 - Styling React Components/01-starting-project 4/src/components/AuthInputs.jsx
+++ Section 6 - Styling React Components/01-starting-project 4/src/components/AuthInputs.jsx
@@ -4,12 +4,12 @@ import { styled } from "styled-components";
 import Input from "./Input";
 import Button from "./Button";
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -32,27 +32,27 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6 ">
           <Input
             label="Email"
             invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
+            // className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
           <Input
             label="Password"
             invalid={passwordNotValid}
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            // className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>
